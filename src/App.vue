9fcfce282e8372933c137fd97a092b358192a414
Longino Bigage <longinobigage@live.com>
2022-09-12 15:05:42 +0300

    removed the error during run time

--- src/App.vue
+++ src/App.vue
@@ -5,34 +5,13 @@
       Hello Contacts
       <ion-icon name="skull-outline" class="animate-pulse text-tertiary pl-1"></ion-icon>
     </h1>
-    <component :is="layout">
-      <slot />
-    </component>
   </div>
 </template>
 
 <script>
-import { markRaw, watch } from 'vue'
-import { useRoute } from 'vue-router'
 
 export default {
   name: 'AppLayout',
-  setup() {
-    const layout = markRaw(AppLayoutDefault)
-    const route = useRoute()
-    watch(
-      () => route.meta,
-      async meta => {
-        try {
-          const component = await import(`@/layouts/${meta.layout}.vue`)
-          layout.value = component?.default || AppLayoutDefault
-        } catch (e) {
-          layout.value = AppLayoutDefault
-        }
-      },
-      { immediate: true }
-    )
-    return { layout }
-  }
+  setup() {  }
 }
 </script>

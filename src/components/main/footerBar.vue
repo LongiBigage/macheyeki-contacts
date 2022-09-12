bb81373ee245e245277a4d46a13caf2c7cc9f734
Longino Bigage <longinobigage@live.com>
2022-09-12 18:07:20 +0300

    Create footerBar.vue

---
+++ src/components/main/footerBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="footer footer-center p-4 bg-base-300 text-primary fixed bottom-0">
+        <div>
+            <p>Copyright © 2022 - All right reserved by MacContacts</p>
+        </div>
+    </footer>
+</template>
